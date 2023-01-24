fbf20d45be1590463b821df2acdeab28a35c6e25
Lagarux <farukguner42@gmail.com>
2023-01-24 15:59:51 +0300

    Added main folders and files

---
+++ CoreAndFood/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace CoreAndFood.Models
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-0E029OM;database=DbCoreFood;integrated security=true");
+        }
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
